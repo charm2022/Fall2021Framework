4124f2a222f09f8c3868e0c3f8878237a2cee77b
charm2022 <cchen28@hotmail.com>
2022-06-18 16:15:44 -0400

    pom.xml

--- Features/Facebook/FacebookLogin.feature
+++ Features/Facebook/FacebookLogin.feature
@@ -24,11 +24,18 @@ Feature: Login
     When I click on login button
     Then I verify user lands on home page
 
-  Scenario: Verify user gets error for invalid credential1
-    When I enter '%^&&*()' as login email
-    When I enter 'abcd@1234' as login password
+  Scenario OUtline: Verify user gets error for invalid credential2
+    When I enter '<invalidEmail>' as login email
+    When I enter '<invalidPassword>' as login password
     When I click on login button
     Then I verify login error is displayed
+
+  Examples:
+    | invalidEmail  | invalidPassword |
+    | BN.com        | abcd@1234       |
+    | %^&&*()       | abcd@1234       |
+  # How to we perform Data Driven Testing (DDT) in Cucumber
+  # ---> using Scenario Outline and Examples
 
     # Background
     # Scenario-1
--- src/test/java/Web/UseDriver.java
+++ src/test/java/Web/UseDriver.java
@@ -1,11 +1,13 @@
 package Web;
 
-import Helper.Misc;
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
+import Helper.DataReader;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class UseDriver {
@@ -28,10 +30,45 @@ public class UseDriver {
      */
    // @Before
     public static void openUrl(){
-        System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
-        driver =  new ChromeDriver();
+
+        DataReader data = new DataReader();
+        String sauceUrl = "https://" + data.getSauceUser()+":"+data.getSauceKey()+"@ondemand.us-west-1.saucelabs.com:443/wd/hub";
+
+        switch (data.getEnv().toLowerCase()) {
+            case "local":
+                switch (data.getBrowserName().toLowerCase()) {
+                    case "chrome":
+                        System.setProperty("webdriver.chrome.driver", data.getChromeDriverPath());
+                        driver =  new ChromeDriver();
+                        break;
+                    case "firefox":
+                        System.setProperty("", data.getFirefoxDriverPath());
+                        driver =  new ChromeDriver();
+                        break;
+                }
+                break;
+            case "sauce":
+                DesiredCapabilities capabilities = null;
+                switch (data.getBrowserName().toLowerCase()) {
+                    case "chrome":
+                        capabilities = DesiredCapabilities.chrome();
+                        break;
+                    case "firefox":
+                        capabilities = DesiredCapabilities.firefox();
+                        break;
+                }
+                capabilities.setCapability("platform", data.getPlatform());
+                capabilities.setCapability("version", data.getVersion());
+
+                try {
+                    driver = new RemoteWebDriver(new URL(sauceUrl), capabilities);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+        }
+
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get ("https://www.facebook.com/");
+        driver.get (data.getUrl());
         // Misc.sleep(2);
 
     }
